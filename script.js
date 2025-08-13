7103d0b1958fb9ae2bad2c50795d0106a1bd8200
Jens Baumeister <jens.baumeister@gmx.net>
2025-08-13 18:05:02 +0200

    button responsive menu modifyed

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function showHideMobileMenu() {
-    document.getElementById("resp_menu").classList.toggle("menu_close");
+    let menu = document.getElementById('resp_menu');
+    let btnImg = document.querySelector('.mobile_menu img');
+
+    menu.classList.toggle('menu_close');
+
+    if (menu.classList.contains('menu_close')) {
+        btnImg.src = './img/icons/menu_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png';
+    } else {btnImg.src = './img/icons/close_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png';
+    }
 }
